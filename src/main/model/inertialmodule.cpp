f7c93669861a3b23805960deafbbd273878aa5dc
tokarev_dr <tokarev_dr@elprib.ru>
2023-10-06 16:23:17 +0300

    Реализовал хранение параметров в мапе

--- src/main/model/inertialmodule.cpp
+++ src/main/model/inertialmodule.cpp
@@ -35,24 +35,24 @@ void InertialModule::set_V_z(double V_z)
     this->V_z = V_z;
 }
 
+void InertialModule::set_W_x(double W_x)
+{
+    this->W_x = W_x;
+}
+
+void InertialModule::set_W_y(double W_y)
+{
+    this->W_y = W_y;
+}
+
+void InertialModule::set_W_z(double W_z)
+{
+    this->W_z = W_z;
+}
+
 void InertialModule::set_omega_circulation(double omega_circulation)
 {
     this->omega_circulation = omega_circulation;
-}
-
-void InertialModule::set_K_0(double K_0)
-{
-    this->K_0 = K_0;
-}
-
-void InertialModule::set_psi_0(double psi_0)
-{
-    this->psi_0 = psi_0;
-}
-
-void InertialModule::set_theta_0(double theta_0)
-{
-    this->theta_0 = theta_0;
 }
 
 void InertialModule::set_K_max(double K_max)
@@ -109,19 +109,14 @@ void InertialModule::set_PSI_theta(double PSI_theta)
     this->PSI_theta = PSI_theta;
 }
 
-void InertialModule::set_phi_0(double phi_0)
+void InertialModule::set_phi(double phi)
 {
-    this->phi_0 = phi_0;
+    this->phi = phi;
 }
 
-void InertialModule::set_lambda_0(double lambda_0)
+void InertialModule::set_lambda(double lambda)
 {
-    this->lambda_0 = lambda_0;
-}
-
-void InertialModule::set_h_0(double h_0)
-{
-    this->h_0 = h_0;
+    this->lambda = lambda;
 }
 
 double InertialModule::get_dt()
@@ -144,24 +139,24 @@ double InertialModule::get_V_z()
     return V_z;
 }
 
+double InertialModule::get_W_x()
+{
+    return W_x;
+}
+
+double InertialModule::get_W_y()
+{
+    return W_y;
+}
+
+double InertialModule::get_W_z()
+{
+    return W_z;
+}
+
 double InertialModule::get_omega_circulation()
 {
     return omega_circulation;
-}
-
-double InertialModule::get_K_0()
-{
-    return K_0;
-}
-
-double InertialModule::get_psi_0()
-{
-    return psi_0;
-}
-
-double InertialModule::get_theta_0()
-{
-    return theta_0;
 }
 
 double InertialModule::get_K_max()
@@ -209,21 +204,6 @@ double InertialModule::get_PSI_theta()
     return PSI_theta;
 }
 
-double InertialModule::get_phi_0()
-{
-    return phi_0;
-}
-
-double InertialModule::get_lambda_0()
-{
-    return lambda_0;
-}
-
-double InertialModule::get_h_0()
-{
-    return h_0;
-}
-
 double InertialModule::get_K()
 {
     return K;
@@ -254,7 +234,7 @@ double InertialModule::get_h()
     return h;
 }
 
-array< array<double, 3>, 3> InertialModule::get_C()
+std::array< std::array<double, 3>, 3> InertialModule::get_C()
 {
     return C;
 }
@@ -284,21 +264,26 @@ double InertialModule::get_V_h()
     return V_h;
 }
 
+QVector<double> InertialModule::getParameter(Parameter param) const
+{
+    return data.value(param);
+}
+
 void InertialModule::handle()
 {
-    size_t point_num = 100000;
+    size_t point_num = 1000000;
 
     for (size_t i = 0; i < point_num; ++i)
     {
-
         R_phi = a * (1 - e_squared) / pow(pow(1 - e_squared * sin(phi), 2.), 3./2) + h;
         R_lambda = a / pow(pow(1 - e_squared * sin(phi), 2.), 1./2) + h;
 
-        K = K + K_0 + omega_circulation * dt + K_max * sin(2*M_PI / T_K * dt + PSI_K);
+        K = K + omega_circulation * dt + K_max * sin(2*M_PI / T_K * dt + PSI_K);
+        K = limits(K, 0, 2*M_PI);
 
-        psi = psi + psi_0 + psi_max * sin(2*M_PI / T_psi * dt + PSI_psi);
+        psi = psi + psi_max * sin(2*M_PI / T_psi * dt + PSI_psi);
 
-        theta = theta + theta_0 + theta_max * sin(2*M_PI / T_theta * dt + PSI_theta);
+        theta = theta + theta_max * sin(2*M_PI / T_theta * dt + PSI_theta);
 
         C[0][0] = cos(K) * cos(theta) + sin(K) * sin(psi) * sin(theta);
         C[0][1] = sin(K) * cos(psi);
@@ -310,6 +295,10 @@ void InertialModule::handle()
         C[2][1] = sin(psi);
         C[2][2] = cos(psi) * cos(theta);
 
+        V_x = V_x + W_x * dt;
+        V_y = V_y + W_y * dt;
+        V_z = V_z + W_z * dt;
+
         V_E = C[0][0] * V_x + C[0][1] *  V_y + C[0][2] * V_z;
         V_N = C[1][0] * V_x + C[1][1] *  V_y + C[1][2] * V_z;
         V_h = C[2][0] * V_x + C[2][1] *  V_y + C[2][2] * V_z;
@@ -318,9 +307,23 @@ void InertialModule::handle()
         lambda = lambda + V_E / (R_lambda * cos(phi)) * dt;
         h = h + V_h * dt;
 
-        time.append(dt*i);
-        data1.append(phi * 180 / M_PI);
-        data2.append(lambda * 180 / M_PI);
+        data[Parameter::t].append(dt*i / 60.);
+        data[Parameter::phi].append(phi * 180 / M_PI);
+        data[Parameter::lambda].append(lambda * 180 / M_PI);
+        data[Parameter::K].append(K * 180 / M_PI);
+        data[Parameter::psi].append(psi * 180 / M_PI);
+        data[Parameter::theta].append(theta * 180 / M_PI);
     }
+}
+
+double InertialModule::limits(double value, double min, double max)
+{
+    while(value > max)
+        value -= max;
+
+    while (value < min)
+        value += max;
+
+    return value;
 }
 
--- src/main/model/inertialmodule.h
+++ src/main/model/inertialmodule.h
@@ -4,6 +4,7 @@
 #include <array>
 
 #include <QVector>
+#include <QMap>
 
 class InertialModule
 {
@@ -11,14 +12,28 @@ public:
     InertialModule();
     ~InertialModule();
 
+    enum class Parameter
+    {
+        t,
+        K,
+        psi,
+        theta,
+        phi,
+        lambda,
+        h,
+        V_E,
+        V_N,
+        V_h
+    };
+
     void set_dt(double dt);
     void set_V_x(double V_x);
     void set_V_y(double V_y);
     void set_V_z(double V_z);
+    void set_W_x(double W_x);
+    void set_W_y(double W_y);
+    void set_W_z(double W_z);
     void set_omega_circulation(double omega_circulation);
-    void set_K_0(double K_0);
-    void set_psi_0(double psi_0);
-    void set_theta_0(double theta_0);
     void set_K_max(double K_max);
     void set_psi_max(double psi_max);
     void set_theta_max(double theta_max);
@@ -28,18 +43,17 @@ public:
     void set_PSI_K(double PSI_K);
     void set_PSI_psi(double PSI_psi);
     void set_PSI_theta(double PSI_theta);
-    void set_phi_0(double phi_0);
-    void set_lambda_0(double lambda_0);
-    void set_h_0(double h_0);
+    void set_phi(double phi);
+    void set_lambda(double lambda);
 
     double get_dt();
     double get_V_x();
     double get_V_y();
     double get_V_z();
+    double get_W_x();
+    double get_W_y();
+    double get_W_z();
     double get_omega_circulation();
-    double get_K_0();
-    double get_psi_0();
-    double get_theta_0();
     double get_K_max();
     double get_psi_max();
     double get_theta_max();
@@ -49,25 +63,26 @@ public:
     double get_PSI_K();
     double get_PSI_psi();
     double get_PSI_theta();
-    double get_phi_0();
-    double get_lambda_0();
-    double get_h_0();
     double get_K();
     double get_psi();
     double get_theta();
     double get_phi();
     double get_lambda();
     double get_h();
-    array< array<double, 3>, 3> get_C();
+    std::array< std::array<double, 3>, 3> get_C();
     double get_R_phi();
     double get_R_lambda();
     double get_V_E();
     double get_V_N();
     double get_V_h();
 
+    QVector<double> getParameter(Parameter param) const;
+
     void handle();
 
 private:
+    double limits(double value, double min, double max);
+
     double dt = 0.01;
 
     double V_x = 0.;
@@ -79,10 +94,6 @@ private:
     double W_z = 0.;
 
     double omega_circulation = 0.;
-
-    double K_0 = 0.;
-    double psi_0 = 0.;
-    double theta_0 = 0.;
 
     double K_max = 0.;
     double psi_max = 0.;
@@ -96,10 +107,6 @@ private:
     double PSI_psi = 0.;
     double PSI_theta = 0.;
 
-    double phi_0 = 0.;
-    double lambda_0 = 0.;
-    double h_0 = 0.;
-
     double K = 0;
     double psi = 0;
     double theta = 0;
@@ -108,7 +115,7 @@ private:
     double lambda = 0.;
     double h = 0.;
 
-    array< array<double, 3>, 3> C[3][3]{};
+    std::array< std::array<double, 3>, 3> C;
 
     const double e_squared = 0.0066934;
     const double a = 6378245;
@@ -120,9 +127,7 @@ private:
     double V_N = 0.;
     double V_h = 0.;
 
-    QVector<double> time;
-    QVector<double> data_phi;
-    QVector<double> data_lambda;
+    QMap< Parameter, QVector<double> > data;
 };
 
 #endif // INERTIALMODULE_H
