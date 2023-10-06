14487cdc2908abaaa009849fd4324920ef297814
tokarev_dr <tokarev_dr@elprib.ru>
2023-10-06 16:23:31 +0300

    Начал реализовывать интерфейс

--- src/main/view/mainwindow.cpp
+++ src/main/view/mainwindow.cpp
@@ -11,6 +11,14 @@ MainWindow::MainWindow(QWidget *parent) :
 
     InertialModule module;
 
+    module.set_V_x(1.);
+    module.set_V_y(1.);
+    //module.set_W_x(0.1);
+    module.set_W_y(0.01);
+    //module.set_omega_circulation(0.05 * M_PI / 180.);
+    module.set_phi(60 * M_PI / 180.);
+    module.set_lambda(30 * M_PI / 180.);
+
     module.handle();
 
     ui->plot->addGraph();
@@ -18,7 +26,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->plot->setInteraction(QCP::iRangeDrag, true);
     ui->plot->setInteraction(QCP::iRangeZoom, true);
 
-    ui->plot->graph(0)->setData(module.time, module.data2);
+    ui->plot->graph(0)->setData(module.getParameter(InertialModule::Parameter::lambda), module.getParameter(InertialModule::Parameter::phi));
 
     ui->plot->rescaleAxes();
 }
